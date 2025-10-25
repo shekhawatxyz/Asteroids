7163515c64d6d1986a5086513de57ae410c1707d
Parakram <94182022+shekhawatxyz@users.noreply.github.com>
2025-10-25 21:59:35 +0530

    initialised display

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from constants import *
 from pygame.locals import *
 
+
 def main():
     pygame.init()
     print(f"Starting Asteroids!")
@@ -9,7 +10,12 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
-        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
