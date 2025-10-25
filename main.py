f5f36fe54fd7204e5353829356ea079d706278b7
Parakram <94182022+shekhawatxyz@users.noreply.github.com>
2025-10-25 21:18:59 +0530

    Add existing Asteroids project files

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+from constants import *
+from pygame.locals import *
+
+def main():
+    pygame.init()
+    print(f"Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        
+
+if __name__ == "__main__":
+    main()
